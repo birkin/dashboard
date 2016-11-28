8f64ef74995baf9e1f851634f3126ce831e2c6ae
birkin <birkin.diana@gmail.com>
2016-11-28 14:49:22 -0500

    modernizes urls.

--- dashboard_app/urls_app.py
+++ dashboard_app/urls_app.py
@@ -2,23 +2,24 @@
 
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, include, url
+from dashboard_app import views
+from django.conf.urls import include, url
 from django.views.generic import RedirectView
 
 
-urlpatterns = patterns('',
+urlpatterns = [
 
-    url( r'^info/$',  'dashboard_app.views.info', name=u'info_url' ),
+    url( r'^info/$',  views.info, name='info_url' ),
 
-    url( r'^request_widget/$',  'dashboard_app.views.request_widget', name=u'request_widget_url' ),
+    url( r'^request_widget/$',  views.request_widget, name='request_widget_url' ),
 
-    url( r'^widget/(?P<identifier>[^/]+)/$',  'dashboard_app.views.widget', name=u'widget_url' ),
+    url( r'^widget/(?P<identifier>[^/]+)/$',  views.widget, name='widget_url' ),
 
-    url( r'^tag/(?P<tag>[^/]+)/$',  'dashboard_app.views.tag', name=u'tag_url' ),
+    url( r'^tag/(?P<tag>[^/]+)/$',  views.tag, name='tag_url' ),
 
-    url( r'^shib_login/$',  'dashboard_app.views.shib_login', name=u'shib_login_url' ),
-    url( r'^logout/$',  'dashboard_app.views.shib_logout', name=u'logout_url' ),
+    url( r'^shib_login/$',  views.shib_login, name='shib_login_url' ),
+    url( r'^logout/$',  views.shib_logout, name='logout_url' ),
 
-    url( r'^$',  RedirectView.as_view(pattern_name=u'info_url') ),
+    url( r'^$',  RedirectView.as_view(pattern_name='info_url') ),
 
-    )
+    ]
