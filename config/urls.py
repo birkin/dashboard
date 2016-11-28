54c5be5efe6ff1a744af1c977186f9bccb198d01
birkin <birkin.diana@gmail.com>
2016-11-28 14:36:16 -0500

    updates urls.py

--- config/urls.py
+++ config/urls.py
@@ -2,16 +2,17 @@
 
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
 
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = [
 
-    url( r'^', include('dashboard_app.urls_app') ),
+    url( r'^admin/', include(admin.site.urls) ),
 
-)
+    url( r'^', include('dashboard_app.urls_app', namespace='dashboard') ),  # eg host/callnumber/anything/
+
+    ]
