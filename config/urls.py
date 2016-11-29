5a9edff097542f02792725c02e9cbed597489558
birkin <birkin.diana@gmail.com>
2016-11-29 14:52:34 -0500

    refactors url handling.

--- config/urls.py
+++ config/urls.py
@@ -2,8 +2,10 @@
 
 from __future__ import unicode_literals
 
+from dashboard_app import views
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 
 admin.autodiscover()
@@ -13,6 +15,17 @@ urlpatterns = [
 
     url( r'^admin/', include(admin.site.urls) ),
 
-    url( r'^', include('dashboard_app.urls_app', namespace='dashboard') ),  # eg host/callnumber/anything/
+    url( r'^info/$',  views.info, name='info_url' ),
+
+    url( r'^request_widget/$',  views.request_widget, name='request_widget_url' ),
+
+    url( r'^widget/(?P<identifier>[^/]+)/$',  views.widget, name='widget_url' ),
+
+    url( r'^tag/(?P<tag>[^/]+)/$',  views.tag, name='tag_url' ),
+
+    url( r'^shib_login/$',  views.shib_login, name='shib_login_url' ),
+    url( r'^logout/$',  views.shib_logout, name='logout_url' ),
+
+    url( r'^$',  RedirectView.as_view(pattern_name='info_url') ),
 
     ]
