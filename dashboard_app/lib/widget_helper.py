0a7497232b9f427a733f80b29dddcbcaf2bdf3be
birkin <birkin.diana@gmail.com>
2016-11-30 09:47:35 -0500

    adds helper class.

---
+++ dashboard_app/lib/widget_helper.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import json
+
+
+class WidgetPrepper(object):
+    """ Assists preparation of views.widgets() data. """
+
+    def __init__(self):
+        pass
+
+    def build_context( self, widget ):
+        """ Preps widget context.
+            Called by views.widget() """
+        return {}
+
+    def build_json_widget_output( self, widget, callback, absolute_uri ):
+        """ Preps json for response.
+            Called by views.widget() """
+        jdict = widget.get_jdict( absolute_uri )
+        output = json.dumps( jdict, sort_keys=True, indent=2 )
+        if callback:
+            output = '{v_callback}({v_output})'.format( v_callback=callback, v_output=output )
+        return output
+
