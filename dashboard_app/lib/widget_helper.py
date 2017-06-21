3ba1a052e2beeabeed3365f252cbda5fadffe4c2
birkin <birkin.diana@gmail.com>
2017-06-21 11:57:31 -0400

    adds escape filter.

--- dashboard_app/lib/widget_helper.py
+++ dashboard_app/lib/widget_helper.py
@@ -29,9 +29,10 @@ class WidgetPrepper(object):
         log.debug( 'data_lst loaded' )
         widget_url = '{scm}://{hst}{url}'.format( scm=scheme, hst=host, url=reverse('widget_url', kwargs={'identifier': widget.slug}) )
         log.debug( 'widget_url, ```{}```'.format(widget_url) )
+
         context = {
             'line_title': widget.title,  # the 'line' title; will appear in legend
-            'chart_title': 'monthly count of easyBorrow requests \\n(disposed of either through a josiah-redirect, or borrowdirect, or illiad)',
+            'chart_title': widget.title_info,
             'dashboard_info': settings.DOCUMENTATION_URL,
             'more_widget_info_url': '',
             'widget_url': widget_url,
@@ -42,6 +43,20 @@ class WidgetPrepper(object):
             'max_y': 4172,  #5% over max count
             # 'max_y': 100000,
             }
+
+        # context = {
+        #     'line_title': widget.title,  # the 'line' title; will appear in legend
+        #     'chart_title': 'monthly count of easyBorrow requests \\n(disposed of either through a josiah-redirect, or borrowdirect, or illiad)',
+        #     'dashboard_info': settings.DOCUMENTATION_URL,
+        #     'more_widget_info_url': '',
+        #     'widget_url': widget_url,
+        #     'widget_data_url': '{}?format=json'.format( widget_url ),
+        #     'data': data_lst,
+        #     'template_row_data': [ [0, 3974], [1, 2923], [2, 3138], [3, 1660], [4, 3631], [5, 3054] ],
+        #     'widget_tick_data': [ "{v:0, f:'2014-09'}", "{v:1, f:'2014-10'}", "{v:2, f:'2014-11'}", "{v:3, f:'2014-12'}", "{v:4, f:'2015-01'}", "{v:5, f:'2015-02'}" ],
+        #     'max_y': 4172,  #5% over max count
+        #     # 'max_y': 100000,
+        #     }
         log.debug( 'context, ```{}```'.format(pprint.pformat(context)) )
         return context
 
