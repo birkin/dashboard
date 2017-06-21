0901ee51ac3a92b6251ab6db342ad1880b7101b9
birkin <birkin.diana@gmail.com>
2017-06-21 13:48:05 -0400

    stubs values-prepper.

--- dashboard_app/lib/widget_helper.py
+++ dashboard_app/lib/widget_helper.py
@@ -38,7 +38,8 @@ class WidgetPrepper(object):
             'widget_url': widget_url,
             'widget_data_url': '{}?format=json'.format( widget_url ),
             'data': data_lst,
-            'template_row_data': [ [0, 3974], [1, 2923], [2, 3138], [3, 1660], [4, 3631], [5, 3054] ],
+            # 'template_row_data': [ [0, 3974], [1, 2923], [2, 3138], [3, 1660], [4, 3631], [5, 3054] ],
+            'template_row_data': self.make_template_row_data( data_lst ),
             'widget_tick_data': [ "{v:0, f:'2014-09'}", "{v:1, f:'2014-10'}", "{v:2, f:'2014-11'}", "{v:3, f:'2014-12'}", "{v:4, f:'2015-01'}", "{v:5, f:'2015-02'}" ],
             'max_y': 4172,  #5% over max count
             # 'max_y': 100000,
@@ -59,6 +60,13 @@ class WidgetPrepper(object):
         #     }
         log.debug( 'context, ```{}```'.format(pprint.pformat(context)) )
         return context
+
+    def make_template_row_data( self, data_lst ):
+        """ Creates the list of sub-lists for context['template_row_data'].
+            Called by build_context() """
+        log.debug( 'data_lst, ```{}```'.format( pprint.pformat(data_lst) ) )
+        template_row_data_lst = [ [0, 3974], [1, 2923], [2, 3138], [3, 1660], [4, 3631], [5, 3054] ]
+        return template_row_data_lst
 
     def build_json_widget_output( self, widget, callback, context ):
         """ Preps json for response.
