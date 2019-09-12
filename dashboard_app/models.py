c56cedf4ea17d999068d53f4fad64cc8c3c20829
birkin <birkin.diana@gmail.com>
2019-09-12 17:10:08 -0400

    removes non-db code from models.

--- dashboard_app/models.py
+++ dashboard_app/models.py
@@ -2,6 +2,7 @@
 
 import datetime, json, logging, os, pprint, itertools
 
+from dashboard_app.lib.widget_processor import WidgetHelper
 from django.conf import settings as project_settings
 from django.core.urlresolvers import reverse
 from django.db import models
@@ -125,36 +126,36 @@ class Widget(models.Model):
     # end class Widget
 
 
-class WidgetHelper( object ):
-    """ Contains helpers for processing Widget() data. """
+# class WidgetHelper( object ):
+#     """ Contains helpers for processing Widget() data. """
 
-    def process_data( self, widget ):
-        """ Ensures data points are valid, and calculates and sets values for certain fields.
-            Called by Widget.save() """
-        self.validate_data( widget.data_points )
-        # widget.baseline_value = lst[0].values()[0]
-        # log.debug( 'hereA' )
-        # widget.best_value = self.get_best_value( widget.best_goal, lst )
-        # widget.current_value = lst[-1].values()[0]
-        # widget.trend_direction = self.get_trend_direction( widget.current_value, lst )
-        # widget.trend_color = self.get_trend_color( widget.trend_direction, widget.best_goal )
-        log.debug( 'dir(widget), `{}`'.format( dir(widget) ) )
-        return widget
+#     def process_data( self, widget ):
+#         """ Ensures data points are valid, and calculates and sets values for certain fields.
+#             Called by Widget.save() """
+#         self.validate_data( widget.data_points )
+#         # widget.baseline_value = lst[0].values()[0]
+#         # log.debug( 'hereA' )
+#         # widget.best_value = self.get_best_value( widget.best_goal, lst )
+#         # widget.current_value = lst[-1].values()[0]
+#         # widget.trend_direction = self.get_trend_direction( widget.current_value, lst )
+#         # widget.trend_color = self.get_trend_color( widget.trend_direction, widget.best_goal )
+#         log.debug( 'dir(widget), `{}`'.format( dir(widget) ) )
+#         return widget
 
-    def validate_data( self, data ):
-        """ Ensures data is a list of dicts.
-            Called by process_data() """
-        validity = False
-        try:
-            lst = json.loads( data )
-            for dct in lst:
-                assert type( dct ) == dict
-            validity = True
-        except Exception as e:
-            log.error( 'error validating data on save; exception, ```{ex}```; problematic data, ```{da}```'.format( ex=e, da=data ) )
-            raise Exception( 'bad_data' )  # needed for model's try-except block to trigger
-        log.debug( 'validity, `{}`'.format(validity) )
-        return validity
+#     def validate_data( self, data ):
+#         """ Ensures data is a list of dicts.
+#             Called by process_data() """
+#         validity = False
+#         try:
+#             lst = json.loads( data )
+#             for dct in lst:
+#                 assert type( dct ) == dict
+#             validity = True
+#         except Exception as e:
+#             log.error( 'error validating data on save; exception, ```{ex}```; problematic data, ```{da}```'.format( ex=e, da=data ) )
+#             raise Exception( 'bad_data' )  # needed for model's try-except block to trigger
+#         log.debug( 'validity, `{}`'.format(validity) )
+#         return validity
 
     # def process_data( self, widget ):
     #     """ Ensures data points are valid, and calculates and sets values for certain fields.
@@ -340,126 +341,126 @@ class WidgetHelper( object ):
 #     # end class ChartMaker
 
 
-class MinichartMaker( object ):
-    """ Contains helpers for creating the minichart. """
+# class MinichartMaker( object ):
+#     """ Contains helpers for creating the minichart. """
 
-    def extract_data_elements( self, lst ):
-        """ Pulls out the middle four elements for crude thumbnail display.
-            Called by SOMETHING. """
-        one_third = int( round( len(lst)/3 ) )
-        two_thirds = int( round( (2*len(lst))/3 ) )
-        thumb_lst = [ lst[0], lst[one_third], lst[two_thirds], lst[-1] ]
-        log.debug( u'in models.MinichartMaker.extract_data_elements(); list, `%s`; thumb_lst validity `%s`' % (lst, thumb_lst) )
-        return thumb_lst
+#     def extract_data_elements( self, lst ):
+#         """ Pulls out the middle four elements for crude thumbnail display.
+#             Called by SOMETHING. """
+#         one_third = int( round( len(lst)/3 ) )
+#         two_thirds = int( round( (2*len(lst))/3 ) )
+#         thumb_lst = [ lst[0], lst[one_third], lst[two_thirds], lst[-1] ]
+#         log.debug( u'in models.MinichartMaker.extract_data_elements(); list, `%s`; thumb_lst validity `%s`' % (lst, thumb_lst) )
+#         return thumb_lst
 
-    # end class MinichartMaker
-
-
-class ShibViewHelper( object ):
-    """ Contains helpers for views.shib_login() """
-
-    def check_shib_headers( self, request ):
-        """ Grabs and checks shib headers, returns boolean.
-            Called by views.shib_login() """
-        shib_checker = ShibChecker()
-        shib_dict = shib_checker.grab_shib_info( request )
-        validity = shib_checker.evaluate_shib_info( shib_dict )
-        log.debug( u'in models.ShibViewHelper.check_shib_headers(); returning validity `%s`' % validity )
-        return ( validity, shib_dict )
-
-    def build_response( self, request, validity, shib_dict, return_url ):
-        """ Sets session vars and redirects to the request page,
-              which will show the citation form on login-success, and a helpful error message on login-failure.
-            Called by views.shib_login() """
-        self.update_session( request, validity, shib_dict )
-        return_response = HttpResponseRedirect( return_url )
-        log.debug( u'in models.ShibViewHelper.build_response(); returning response' )
-        return return_response
-
-    def update_session( self, request, validity, shib_dict ):
-        request.session[u'shib_login_error'] = validity  # boolean
-        request.session[u'authorized'] = validity
-        if validity:
-            request.session[u'user_info'] = {
-                u'name': u'%s %s' % ( shib_dict[u'firstname'], shib_dict[u'lastname'] ),
-                u'email': shib_dict[u'email'],
-                u'patron_barcode': shib_dict[u'patron_barcode'] }
-            request.session[u'shib_login_error'] = False
-        return
-
-    # end class ShibViewHelper
+#     # end class MinichartMaker
 
 
-class ShibChecker( object ):
-    """ Contains helpers for checking Shib. """
+# class ShibViewHelper( object ):
+#     """ Contains helpers for views.shib_login() """
 
-    def __init__( self ):
-        self.TEST_SHIB_JSON = os.environ.get( u'DSHBRD__TEST_SHIB_JSON', u'' )
-        self.SHIB_REQUIRED_GROUP = os.environ[u'DSHBRD__SHIB_REQUIRED_GROUP']
+#     def check_shib_headers( self, request ):
+#         """ Grabs and checks shib headers, returns boolean.
+#             Called by views.shib_login() """
+#         shib_checker = ShibChecker()
+#         shib_dict = shib_checker.grab_shib_info( request )
+#         validity = shib_checker.evaluate_shib_info( shib_dict )
+#         log.debug( u'in models.ShibViewHelper.check_shib_headers(); returning validity `%s`' % validity )
+#         return ( validity, shib_dict )
 
-    def grab_shib_info( self, request ):
-        """ Grabs shib values from http-header or dev-settings.
-            Called by models.ShibViewHelper.check_shib_headers() """
-        shib_dict = {}
-        if u'Shibboleth-eppn' in request.META:
-            shib_dict = self.grab_shib_from_meta( request )
-        else:
-            if request.get_host() == u'127.0.0.1' and project_settings.DEBUG == True:
-                shib_dict = json.loads( self.TEST_SHIB_JSON )
-        log.debug( u'in models.ShibChecker.grab_shib_info(); shib_dict is: %s' % pprint.pformat(shib_dict) )
-        return shib_dict
+#     def build_response( self, request, validity, shib_dict, return_url ):
+#         """ Sets session vars and redirects to the request page,
+#               which will show the citation form on login-success, and a helpful error message on login-failure.
+#             Called by views.shib_login() """
+#         self.update_session( request, validity, shib_dict )
+#         return_response = HttpResponseRedirect( return_url )
+#         log.debug( u'in models.ShibViewHelper.build_response(); returning response' )
+#         return return_response
 
-    def grab_shib_from_meta( self, request ):
-        """ Extracts shib values from http-header.
-            Called by grab_shib_info() """
-        shib_dict = {
-            u'eppn': request.META.get( u'Shibboleth-eppn', u'' ),
-            u'firstname': request.META.get( u'Shibboleth-givenName', u'' ),
-            u'lastname': request.META.get( u'Shibboleth-sn', u'' ),
-            u'email': request.META.get( u'Shibboleth-mail', u'' ).lower(),
-            u'patron_barcode': request.META.get( u'Shibboleth-brownBarCode', u'' ),
-            u'member_of': request.META.get( u'Shibboleth-isMemberOf', u'' ) }
-        return shib_dict
+#     def update_session( self, request, validity, shib_dict ):
+#         request.session[u'shib_login_error'] = validity  # boolean
+#         request.session[u'authorized'] = validity
+#         if validity:
+#             request.session[u'user_info'] = {
+#                 u'name': u'%s %s' % ( shib_dict[u'firstname'], shib_dict[u'lastname'] ),
+#                 u'email': shib_dict[u'email'],
+#                 u'patron_barcode': shib_dict[u'patron_barcode'] }
+#             request.session[u'shib_login_error'] = False
+#         return
 
-    def evaluate_shib_info( self, shib_dict ):
-        """ Returns boolean.
-            Called by models.ShibViewHelper.check_shib_headers() """
-        validity = False
-        if self.all_values_present(shib_dict) and self.brown_user_confirmed(shib_dict) and self.eresources_allowed(shib_dict):
-            validity = True
-        log.debug( u'in models.ShibChecker.evaluate_shib_info(); validity, `%s`' % validity )
-        return validity
+#     # end class ShibViewHelper
 
-    def all_values_present( self, shib_dict ):
-        """ Returns boolean.
-            Called by evaluate_shib_info() """
-        present_check = False
-        if sorted( shib_dict.keys() ) == [u'email', u'eppn', u'firstname', u'lastname', u'member_of', u'patron_barcode']:
-            value_test = u'init'
-            for (key, value) in shib_dict.items():
-                if len( value.strip() ) == 0:
-                    value_test = u'fail'
-            if value_test == u'init':
-                present_check = True
-        log.debug( u'in models.ShibChecker.all_values_present(); present_check, `%s`' % present_check )
-        return present_check
 
-    def brown_user_confirmed( self, shib_dict ):
-        """ Returns boolean.
-            Called by evaluate_shib_info() """
-        brown_check = False
-        if u'@brown.edu' in shib_dict[u'eppn']:
-            brown_check = True
-        log.debug( u'in models.ShibChecker.brown_user_confirmed(); brown_check, `%s`' % brown_check )
-        return brown_check
+# class ShibChecker( object ):
+#     """ Contains helpers for checking Shib. """
 
-    def eresources_allowed( self, shib_dict ):
-        """ Returns boolean.
-            Called by evaluate_shib_info() """
-        eresources_check = False
-        if self.SHIB_REQUIRED_GROUP in shib_dict[u'member_of']:
-            eresources_check = True
-        log.debug( u'in models.ShibChecker.eresources_allowed(); eresources_check, `%s`' % eresources_check )
-        return eresources_check
+#     def __init__( self ):
+#         self.TEST_SHIB_JSON = os.environ.get( u'DSHBRD__TEST_SHIB_JSON', u'' )
+#         self.SHIB_REQUIRED_GROUP = os.environ[u'DSHBRD__SHIB_REQUIRED_GROUP']
 
-    # end class ShibChecker
+#     def grab_shib_info( self, request ):
+#         """ Grabs shib values from http-header or dev-settings.
+#             Called by models.ShibViewHelper.check_shib_headers() """
+#         shib_dict = {}
+#         if u'Shibboleth-eppn' in request.META:
+#             shib_dict = self.grab_shib_from_meta( request )
+#         else:
+#             if request.get_host() == u'127.0.0.1' and project_settings.DEBUG == True:
+#                 shib_dict = json.loads( self.TEST_SHIB_JSON )
+#         log.debug( u'in models.ShibChecker.grab_shib_info(); shib_dict is: %s' % pprint.pformat(shib_dict) )
+#         return shib_dict
+
+#     def grab_shib_from_meta( self, request ):
+#         """ Extracts shib values from http-header.
+#             Called by grab_shib_info() """
+#         shib_dict = {
+#             u'eppn': request.META.get( u'Shibboleth-eppn', u'' ),
+#             u'firstname': request.META.get( u'Shibboleth-givenName', u'' ),
+#             u'lastname': request.META.get( u'Shibboleth-sn', u'' ),
+#             u'email': request.META.get( u'Shibboleth-mail', u'' ).lower(),
+#             u'patron_barcode': request.META.get( u'Shibboleth-brownBarCode', u'' ),
+#             u'member_of': request.META.get( u'Shibboleth-isMemberOf', u'' ) }
+#         return shib_dict
+
+#     def evaluate_shib_info( self, shib_dict ):
+#         """ Returns boolean.
+#             Called by models.ShibViewHelper.check_shib_headers() """
+#         validity = False
+#         if self.all_values_present(shib_dict) and self.brown_user_confirmed(shib_dict) and self.eresources_allowed(shib_dict):
+#             validity = True
+#         log.debug( u'in models.ShibChecker.evaluate_shib_info(); validity, `%s`' % validity )
+#         return validity
+
+#     def all_values_present( self, shib_dict ):
+#         """ Returns boolean.
+#             Called by evaluate_shib_info() """
+#         present_check = False
+#         if sorted( shib_dict.keys() ) == [u'email', u'eppn', u'firstname', u'lastname', u'member_of', u'patron_barcode']:
+#             value_test = u'init'
+#             for (key, value) in shib_dict.items():
+#                 if len( value.strip() ) == 0:
+#                     value_test = u'fail'
+#             if value_test == u'init':
+#                 present_check = True
+#         log.debug( u'in models.ShibChecker.all_values_present(); present_check, `%s`' % present_check )
+#         return present_check
+
+#     def brown_user_confirmed( self, shib_dict ):
+#         """ Returns boolean.
+#             Called by evaluate_shib_info() """
+#         brown_check = False
+#         if u'@brown.edu' in shib_dict[u'eppn']:
+#             brown_check = True
+#         log.debug( u'in models.ShibChecker.brown_user_confirmed(); brown_check, `%s`' % brown_check )
+#         return brown_check
+
+#     def eresources_allowed( self, shib_dict ):
+#         """ Returns boolean.
+#             Called by evaluate_shib_info() """
+#         eresources_check = False
+#         if self.SHIB_REQUIRED_GROUP in shib_dict[u'member_of']:
+#             eresources_check = True
+#         log.debug( u'in models.ShibChecker.eresources_allowed(); eresources_check, `%s`' % eresources_check )
+#         return eresources_check
+
+#     # end class ShibChecker
